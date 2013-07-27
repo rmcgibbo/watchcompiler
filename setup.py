3503d7871f527fdc69609c2aa86a5aaa1483bde1
Thomas Lane <tjlane@titan-ext7.ccs.ornl.gov>
2013-07-27 01:29:56 -0400

    Removed dependence on watchdog, which doesnt seem to work on cray

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 import sys
 from distutils.core import setup
 
-try:
-    import watchdog
-except:
-    breaker = '#' * 80
-    print breaker
-    print 'This package required the python "watchdog" module'
-    print breaker
-    sys.exit(1)
-
 setup(name='watchcompiler',
       version='0.1',
       description='Watch a directory and trigger compilation of new kernels',
